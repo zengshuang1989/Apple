a85f4c99ce462a4e64fa93f1b5d94f9c68f16a74
zeng shuang <57359931@qq.com>
2017-09-04 00:55:54 +0800

    Failed to install database
    Failed to install database

--- sql/apple.sql
+++ sql/apple.sql
@@ -1,1 +1,10 @@
 /* Start our own program from the database script : ) */
+/* 2017/09/04 I got stuck on the installation database. I am embarrassed.
+
+when I change the file my.ini, net start fail. The error log:
+
+170904  0:17:43  InnoDB: Started; log sequence number 0 43655
+170904  0:17:43 [Note] Recovering after a crash using mysql-bin
+170904  0:17:43 [Note] Starting crash recovery...
+170904  0:17:43 [Note] Crash recovery finished.
+170904  0:17:44 [ERROR] Fatal error: Can't open and lock privilege tables: Table 'mysql.host' doesn't exist. */
