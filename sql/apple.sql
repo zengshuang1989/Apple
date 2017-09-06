fc80c99d7c5d5371dae64d4419e1a65fafe21307
zeng shuang <57359931@qq.com>
2017-09-07 00:13:44 +0800

    创建表结构
    创建表结构

--- sql/apple.sql
+++ sql/apple.sql
@@ -1,10 +1,67 @@
-/* Start our own program from the database script : ) */
-/* 2017/09/04 I got stuck on the installation database. I am embarrassed.
 
-when I change the file my.ini, net start fail. The error log:
+drop user 'apple'@'localhost';
 
-170904  0:17:43  InnoDB: Started; log sequence number 0 43655
-170904  0:17:43 [Note] Recovering after a crash using mysql-bin
-170904  0:17:43 [Note] Starting crash recovery...
-170904  0:17:43 [Note] Crash recovery finished.
-170904  0:17:44 [ERROR] Fatal error: Can't open and lock privilege tables: Table 'mysql.host' doesn't exist. */
+/*
+创建schema
+*/
+drop database if exists apple;
+create database apple character set utf8;
+
+/*
+创建用户
+*/
+create user 'apple'@'localhost' identified by 'apple';
+GRANT all privileges ON apple.* TO 'apple'@'localhost'; 
+
+/*
+使用schema
+*/
+use apple;
+
+/*帐户表*/
+
+DROP TABLE IF EXISTS account;
+CREATE TABLE account (
+  ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(128) NOT NULL,
+  type VARCHAR(128) NOT NULL,
+  balance DOUBLE NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+/*项目表*/
+
+DROP TABLE IF EXISTS item;
+CREATE TABLE item (
+  ID INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  pid INTEGER UNSIGNED,
+  type VARCHAR(16) NOT NULL,
+  PRIMARY KEY (ID)
+);
+/*
+记录表
+*/
+DROP TABLE IF EXISTS record;
+CREATE TABLE record (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  item_id INTEGER UNSIGNED NOT NULL,
+  acct_id INTEGER UNSIGNED NOT NULL,
+  amount DOUBLE NOT NULL,
+  remark VARCHAR(4000),
+  time DATETIME NOT NULL,
+  PRIMARY KEY (id)
+);
+/*
+转帐表
+*/
+DROP TABLE IF EXISTS transfer;
+CREATE TABLE transfer (
+  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  source_acct_id INTEGER UNSIGNED NOT NULL,
+  target_acct_id INTEGER UNSIGNED NOT NULL,
+  amount DOUBLE NOT NULL,
+  remark VARCHAR(4000),
+  time DATETIME NOT NULL,
+  PRIMARY KEY (id)
+);
