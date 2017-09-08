7a0907edc43bb791b3fd653879e48cdb28bba483
rayzeng27 <31339078+rayzeng27@users.noreply.github.com>
2017-09-08 17:26:18 +0800

    Update apple.sql

--- sql/apple.sql
+++ sql/apple.sql
@@ -1,10 +1,10 @@
 
 /* 创建schema */
-drop database if exists apple;
+drop   database if exists apple;
 create database apple character set utf8;
 
 /* 创建用户 */
-drop user 'banana'@'localhost';
+drop   user 'banana'@'localhost';
 create user 'banana'@'localhost' identified by 'banana';
 GRANT all privileges ON apple.* TO 'banana'@'localhost'; 
 
@@ -34,12 +34,13 @@ CREATE TABLE category (
 /* 记录表 */
 DROP TABLE IF EXISTS record;
 CREATE TABLE record (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  item_id INTEGER UNSIGNED NOT NULL,
-  flowout_acct_id INTEGER UNSIGNED,
-  flowin_acct_id INTEGER UNSIGNED,
-  money DOUBLE NOT NULL,
-  time DATETIME NOT NULL,
-  remark VARCHAR(4000),
+  id INTEGER        UNSIGNED NOT NULL AUTO_INCREMENT,
+  category_id       INTEGER UNSIGNED NOT NULL,
+  category_type     VARCHAR(16) NOT NULL, /* 从category表上冗余过来的字段，便于统计 */
+  flowout_acct_id   INTEGER UNSIGNED,
+  flowin_acct_id    INTEGER UNSIGNED,
+  money DOUBLE      NOT NULL,
+  time DATETIME     NOT NULL,
+  remark            VARCHAR(4000),
   PRIMARY KEY (id)
 );
